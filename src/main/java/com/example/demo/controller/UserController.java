6d2f820c3310fe57ef1cde602f24b7a3b9efce67
ivan.korenevsky <bugnyN-6vosse-vuqfuf>
2020-05-02 16:17:25 +0300

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("user-add")
+    public String addUserForm(User user){
+        return "user-add";
+    }
+
+    @PostMapping()
+    public String addUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
